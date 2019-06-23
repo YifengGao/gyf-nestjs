101510031fa32b67797494d4a8726c7f3737b7bb
YifengGao <yifeng@jiacz.com>
2019-06-23 16:44:28 +0800

    任务7：注入依赖的简单形式

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -7,22 +7,20 @@ import { Post } from 'src/posts/interfaces/post.interface';
 
 @Controller('posts')
 export class PostsController {
-  private readonly demoService;
-
-  constructor(demoService: DemoService){
-    this.demoService = demoService;
-  }
+  constructor(private readonly demoService: DemoService){}
 
   @Get()
   index(){
     return this.demoService.findAll();
   }
+
   @Get(':id')
   show(@Param() params){
     return{
       title: `Post ${params.id}`
     }
   }
+  
   @Post()
   store(@Body() post: CreatePostDto) {
     this.demoService.create(post);
