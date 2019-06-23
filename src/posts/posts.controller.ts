48cd52a3f8fbc5e5d912cfd7de73a4dcfb7eb37a
YifengGao <yifeng@jiacz.com>
2019-06-23 16:32:25 +0800

    任务6：使用服务

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,18 +1,21 @@
 import { Controller, Get, Req, Query, Headers, Param, Post, Body} from '@nestjs/common';
 import { Request } from 'express';
 import { CreatePostDto } from './post.dto';
+import { DemoService } from './providers/demo/demo.service';
+import { Post } from 'src/posts/interfaces/post.interface';
+
 
 @Controller('posts')
 export class PostsController {
+  private readonly demoService;
+
+  constructor(demoService: DemoService){
+    this.demoService = demoService;
+  }
+
   @Get()
-  index(@Headers('authorization') headers){
-    console.log(headers)
-    
-    return [
-      {
-        title: 'hello ~'
-      }
-    ];
+  index(){
+    return this.demoService.findAll();
   }
   @Get(':id')
   show(@Param() params){
@@ -22,6 +25,6 @@ export class PostsController {
   }
   @Post()
   store(@Body() post: CreatePostDto) {
-    console.log(post.title);
+    this.demoService.create(post);
   }
 }
